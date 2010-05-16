80fb500409f7b3ebf70e59faafe603a1c0c27a62
dakrone <lee@writequit.org>
2010-05-15 22:28:40 -0600

    initial commit of verify-all macro and test.

--- src/org/dipert/mockito.clj
+++ src/org/dipert/mockito.clj
@@ -30,8 +30,21 @@
 ;        (verify-2 klass clear)      =>  (.clear (org.mockito.Mockito/verify klass))
 
 (defmacro verify-2
-  "Verifies that call(s) were made to mocked"
+  "Verifies that call(s) were made to mocked. Arugments to the verified
+  method are optional.
+
+  usage:
+    (verify-2 klass method arg1 arg2 arg3 ...)"
   [mocked call & args]
   (if (nil? args)
     (list* (symbol (str \. call)) `((Mockito/verify ~mocked)))
     (list* (symbol (str \. call)) `(Mockito/verify ~mocked) args)))
+
+
+(defmacro verify-all
+  "Verifies that all call(s) were made to mocked
+
+  usage:
+    (verify-all klass [method1 arg1 arg2] [method2 arg1] [method3])"
+  [klass & methodlists]
+  `(do ~@(map #(list* `(verify-2 ~klass ~(first %) ~@(rest %))) methodlists)))
--- test/org/dipert/mockito_test.clj
+++ test/org/dipert/mockito_test.clj
@@ -3,15 +3,34 @@
   (:require [org.dipert.mockito :as mockito])
   (:import [java.util ArrayList]))
 
+
 (deftest test-stub-method
          (let [mocklist (mockito/mock java.util.ArrayList)]
            (mockito/stub-method mocklist get [0] :returning "first")
            (is (= (.get mocklist 0)
                   "first"))))
 
+
 (deftest test-verify
          (let [mocklist (mockito/mock java.util.ArrayList)]
            (.add mocklist "one")
            (.clear mocklist)
            (mockito/verify-2 mocklist add "one")
+           (is (thrown? org.mockito.exceptions.verification.WantedButNotInvoked
+                        (mockito/verify-2 mocklist add "two")))
            (mockito/verify-2 mocklist clear)))
+
+
+(deftest test-verify-all
+         (let [mocklist (mockito/mock java.util.ArrayList)]
+           (.add mocklist "one")
+           (.add mocklist "two")
+           (.clear mocklist)
+           (mockito/verify-all mocklist
+                               [add "one"]
+                               [add "two"]
+                               [clear])
+           (is (thrown? org.mockito.exceptions.verification.WantedButNotInvoked
+                        (mockito/verify-all mocklist
+                                            [add "three"]
+                                            [clear])))))
